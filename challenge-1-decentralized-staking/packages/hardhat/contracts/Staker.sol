d664af6e2fb2298a69acb02c5a261c4128e691e7
Jason Graves <godlikemouse@godlikemouse.com>
2023-03-14 00:58:49 -0600

    Removed yarn.lock from root, minor code fixes

--- challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
+++ challenge-1-decentralized-staking/packages/hardhat/contracts/Staker.sol
@@ -4,11 +4,19 @@ pragma solidity 0.8.4;
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
-//TODO: Update the interest mechanism in the Staker.sol contract so that you receive a "non-linear" amount of ETH based on the blocks between deposit and withdrawal (I suggest implementing a basic exponential function!)
-//TODO: Allow users to deposit any arbitrary amount of ETH into the smart contract, not just 0.5 ETH.
-//TODO: Instead of using the vanilla ExampleExternalContract contract, implement a function in Staker.sol that allows you to retrieve the ETH locked up in ExampleExternalContract and re-deposit it back into the Staker contract.
-//  Make sure to only "white-list" a single address to call this new function to gate its usage!
-//  Make sure that you create logic/remove existing code to ensure that users are able to interact with the Staker contract over and over again! We want to be able to ping-pong from Staker -> ExampleExternalContract repeatedly!
+//TODO: Update the interest mechanism in the Staker.sol contract so that you
+//  receive a "non-linear" amount of ETH based on the blocks between deposit and
+//  withdrawal (I suggest implementing a basic exponential function!)
+//TODO: Allow users to deposit any arbitrary amount of ETH into the smart
+//  contract, not just 0.5 ETH.
+//TODO: Instead of using the vanilla ExampleExternalContract contract, implement
+//  a function in Staker.sol that allows you to retrieve the ETH locked up in
+//  ExampleExternalContract and re-deposit it back into the Staker contract.
+//  Make sure to only "white-list" a single address to call this new function to
+//  gate its usage!
+//  Make sure that you create logic/remove existing code to ensure that users
+//  are able to interact with the Staker contract over and over again! We want
+//  to be able to ping-pong from Staker -> ExampleExternalContract repeatedly!
 
 contract Staker {
     ExampleExternalContract public exampleExternalContract;
@@ -27,9 +35,8 @@ contract Staker {
     event Execute(address indexed sender, uint256 amount);
 
     // Modifiers
-    /*
-  Checks if the withdrawal period has been reached or not
-  */
+
+    // Checks if the withdrawal period has been reached or not
     modifier withdrawalDeadlineReached(bool requireReached) {
         uint256 timeRemaining = withdrawalTimeLeft();
         if (requireReached) {
@@ -40,9 +47,7 @@ contract Staker {
         _;
     }
 
-    /*
-  Checks if the claim period has ended or not
-  */
+    // Checks if the claim period has ended or not
     modifier claimDeadlineReached(bool requireReached) {
         uint256 timeRemaining = claimPeriodLeft();
         if (requireReached) {
@@ -53,9 +58,7 @@ contract Staker {
         _;
     }
 
-    /*
-  Requires that the contract only be completed once!
-  */
+    // Requires that the contract only be completed once!
     modifier notCompleted() {
         bool completed = exampleExternalContract.completed();
         require(!completed, "Stake already completed!");
@@ -75,15 +78,13 @@ contract Staker {
         withdrawalDeadlineReached(false)
         claimDeadlineReached(false)
     {
-        balances[msg.sender] = balances[msg.sender] + msg.value;
+        balances[msg.sender] *= msg.value;
         depositTimestamps[msg.sender] = block.timestamp;
         emit Stake(msg.sender, msg.value);
     }
 
-    /*
-  Withdraw function for a user to remove their staked ETH inclusive
-  of both principal and any accrued interest
-  */
+    // Withdraw function for a user to remove their staked ETH inclusive
+    // of both principal and any accrued interest
     function withdraw()
         public
         withdrawalDeadlineReached(true)
@@ -104,52 +105,42 @@ contract Staker {
         require(sent, "RIP; withdrawal failed :( ");
     }
 
-    /*
-  Allows any user to repatriate "unproductive" funds that are left in the staking contract
-  past the defined withdrawal period
-  */
+    // Allows any user to repatriate "unproductive" funds that are left in the
+    // staking contract past the defined withdrawal period
     function execute() public claimDeadlineReached(true) notCompleted {
         uint256 contractBalance = address(this).balance;
         exampleExternalContract.complete{value: address(this).balance}();
     }
 
-    /*
-  READ-ONLY function to calculate the time remaining before the minimum staking period has passed
-  */
+    // READ-ONLY function to calculate the time remaining before the minimum
+    // staking period has passed
     function withdrawalTimeLeft()
         public
         view
         returns (uint256 withdrawalTimeLeft)
     {
-        if (block.timestamp >= withdrawalDeadline) {
-            return (0);
-        } else {
-            return (withdrawalDeadline - block.timestamp);
-        }
+        return
+            block.timestamp >= withdrawalDeadline
+                ? 0
+                : (withdrawalDeadline - block.timestamp);
     }
 
-    /*
-  READ-ONLY function to calculate the time remaining before the minimum staking period has passed
-  */
+    // READ-ONLY function to calculate the time remaining before the minimum
+    // staking period has passed
     function claimPeriodLeft() public view returns (uint256 claimPeriodLeft) {
-        if (block.timestamp >= claimDeadline) {
-            return (0);
-        } else {
-            return (claimDeadline - block.timestamp);
-        }
+        return
+            block.timestamp >= claimDeadline
+                ? 0
+                : (claimDeadline - block.timestamp);
     }
 
-    /*
-  Time to "kill-time" on our local testnet
-  */
+    // Time to "kill-time" on our local testnet
     function killTime() public {
         currentBlock = block.timestamp;
     }
 
-    /*
-  \Function for our smart contract to receive ETH
-  cc: https://docs.soliditylang.org/en/latest/contracts.html#receive-ether-function
-  */
+    // Function for our smart contract to receive ETH
+    // cc: https://docs.soliditylang.org/en/latest/contracts.html#receive-ether-function
     receive() external payable {
         emit Received(msg.sender, msg.value);
     }
